59e8dd8901b1009b13e0df708a0b75687a2e974c
AndreiChornii <andrei.chornii@gmail.com>
2020-04-16 17:13:54 +0300

    before_mysql_administration_page_521_v_2

--- classes/Ijdb/Controllers/Joke.php
+++ classes/Ijdb/Controllers/Joke.php
@@ -59,7 +59,7 @@ class Joke {
         $author = $this->authentication->getUser();
         $joke = $this->jokesTable->findById($_POST['id']);
 
-        if ($joke['authorId'] != $author['id']) {
+        if ($joke['authorid'] != $author['id']) {
             return;
         }
 
@@ -73,14 +73,15 @@ class Joke {
 
         if (isset($_GET['id'])) {
             $joke = $this->jokesTable->findById($_GET['id']);
-            if ($joke['authorId'] != $author['id']) {
+
+            if ($joke['authorid'] != $author['id']) {
                 return;
             }
         }
-
+        
         $joke = $_POST['joke'];
         $joke['jokedate'] = new \DateTime();
-        $joke['authorId'] = $author['id'];
+        $joke['authorid'] = $author['id'];
 
         $this->jokesTable->save($joke);
 
