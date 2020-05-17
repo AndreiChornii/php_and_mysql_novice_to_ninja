86bec7c7cba559bbc3d9d4c51a2679bf11729bb0
AndreiChornii <andrei.chornii@gmail.com>
2020-05-17 21:16:55 +0300

    till_p_610_585

--- classes/Ijdb/Entity/Joke.php
+++ classes/Ijdb/Entity/Joke.php
@@ -27,8 +27,18 @@ class Joke {
 
     public function addCategory($categoryId) {
         $jokeCat = ['jokeId' => $this->id,
-                    'categoryId' => $categoryId];
+            'categoryId' => $categoryId];
         $this->jokeCategoriesTable->save($jokeCat);
+    }
+
+    public function hasCategory($categoryId) {
+        $jokeCategories = $this->jokeCategoriesTable->
+                find('jokeId', $this->id);
+        foreach ($jokeCategories as $jokeCategory) {
+            if ($jokeCategory->categoryId == $categoryId) {
+                return true;
+            }
+        }
     }
 
 }
