aa354fe68b00fa8d193dffd48c2e484178096921
AndreiChornii <andrei.chornii@gmail.com>
2020-05-24 11:53:45 +0300

    till_p_625_600

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -16,6 +16,7 @@
                 <li><a href="/joke/edit">Add a new Joke</a></li>
                 <li><a href="/author/register">Register new author</a></li>
                 <li><a href="/category/list">Categories list</a></li>
+                <li><a href="/author/list">Users list</a></li>
                 <?php if ($loggedIn): ?>
                     <li><a href="/logout">Log out</a>
                     </li>
